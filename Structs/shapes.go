a0a7222c84976f4d289a1c4fcdaa2e027d57b49b
kuro5888 <ben.allan5888@gmail.com>
2024-03-02 11:45:06 +0000

    Added rectangle struct to make things make more sense

--- Structs/shapes.go
+++ Structs/shapes.go
@@ -1,5 +1,16 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height) 
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+// Calculate Perimeter of a rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height) 
+}
+
+// Calculate Area of a rectangle
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
