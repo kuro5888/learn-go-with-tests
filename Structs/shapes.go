57d3d78d6276e6ebc39bbf9b35317ee0ce3faa5e
kuro5888 <ben.allan5888@gmail.com>
2024-03-02 12:08:26 +0000

    Added interface for shapes

--- Structs/shapes.go
+++ Structs/shapes.go
@@ -1,16 +1,31 @@
 package main
 
-type Rectangle struct {
-	Width float64
-	Height float64
-}
+import "math"
 
 // Calculate Perimeter of a rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height) 
 }
 
-// Calculate Area of a rectangle
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+
+type Circle struct {
+	Radius float64
+}
+
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
